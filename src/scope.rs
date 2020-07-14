5056e92cf8a8d3c421f3a2550069a750a3f7c231
Mark <mverleg.noreply@gmail.com>
2020-07-14 14:06:37 +0200

    RootScope initialization, and move RootScope field to Scope instead of ScopeData

--- src/scope.rs
+++ src/scope.rs
@@ -1,43 +1,72 @@
+use ::std::borrow::BorrowMut;
+use ::std::cell::RefCell;
 use ::std::collections::HashMap;
 use ::std::rc::Rc;
 
+use ::string_interner::StringInterner;
+
 use crate::name::Name;
-use std::borrow::BorrowMut;
 
 #[derive(Debug, Clone)]
-pub struct Scope {
-    data: Rc<ScopeContainer>,
+pub struct RootScope {
+    // This prevents us from needing
+    root_data: Rc<RootScopeData>,
+    // Note: this is only optional to be able to initialize the circular reference.
+    // After initialization it should always be Some (accessible with .scope() method).
+    scope: Option<Scope>,
 }
 
 #[derive(Debug)]
-struct ScopeContainer {
-    parent: Option<Scope>,
-    children: Vec<Scope>,
-    names: HashMap<String, Name>,
+struct RootScopeData {
+    //TODO @mark: names & interning incomplete
+    names: RefCell<StringInterner<usize>>,
+    scopes: RefCell<Vec<ScopeData>>,
+}
+
+impl RootScope {
+    pub fn new() -> Self {
+        // Note: while this constructor is running, the object is invalid state.
+        let mut root = RootScope {
+            root_data: Rc::new(RootScopeData {
+                names: RefCell::new(StringInterner::new()),
+                scopes: RefCell::new(vec![]),
+            }),
+            scope: None,
+        };
+        let scope_data = ScopeData {
+            parent: None,
+            children: vec![],
+        };
+        root.root_data.scopes.borrow_mut().push(scope_data);
+        root.scope = Some(Scope {
+            root: root.clone(),
+            index: 0,
+        });
+        //TODO @mark: check that the scope resolves
+        debug_assert!(root.scope());
+        root
+    }
+
+    pub fn scope(&self) -> &Scope {
+        &self.scope.unwrap()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Scope {
+    root: RootScope,
+    index: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct ScopeData {
+    parent: Option<usize>,
+    children: Vec<usize>,
 }
 
 impl Scope {
-    pub fn new_root() -> Self {
-        Scope {
-            data: Rc::new(ScopeContainer {
-                parent: None,
-                children: vec![],
-                names: HashMap::new(),
-            })
-        }
-    }
-
-    pub fn add_child(&mut self) -> Self {
-        let parent: Scope = (*self).clone();
-        let child = Scope {
-            data: Rc::new(ScopeContainer {
-                parent: Some(parent),
-                children: vec![],
-                names: HashMap::new(),
-            })
-        };
-        self.data.borrow_mut().children.push(child.clone());
-        child
+    pub fn add_child(&self) -> Self {
+        unimplemented!()  //TODO @mark: TEMPORARY! REMOVE THIS!
     }
 }
 
@@ -47,6 +76,6 @@ mod tests {
 
     #[test]
     fn create_root() {
-        Scope::new_root();
+        RootScope::new();
     }
 }
