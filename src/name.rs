076a0f2f6b11a5b1153a00ba2b9d8a93661940e7
Mark <mverleg.noreply@gmail.com>
2020-07-14 23:18:57 +0200

    Re-add anon PartialEq method (realized Eq is wrong) for tests

--- src/name.rs
+++ src/name.rs
@@ -5,10 +5,19 @@ use crate::scope::Scope;
 /// An identifier, either anonymous or given.
 ///
 /// Instances should be created through `Scope`.
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Name {
     pub(crate) scope: Scope,
     pub(crate) data: InputName,
+}
+
+impl Name {
+    pub fn unwrap_given(self) -> GivenName {
+        match self.data {
+            InputName::Given(given) => given,
+            InputName::Anonymous(_) => panic!("unwrap_given on an anonymous name"),
+        }
+    }
 }
 
 /// A given identifier that should not collide within a scope.
@@ -16,6 +25,18 @@ pub struct Name {
 pub struct GivenName {
     // Index in the scope's string 'arena'.
     pub(crate) index: usize,
+}
+
+impl PartialEq for GivenName {
+    fn eq(&self, other: &Self) -> bool {
+        self.index == other.index
+    }
+}
+
+impl hash::Hash for GivenName {
+    fn hash<H: hash::Hasher>(&self, state: &mut H) {
+        self.index.hash(state)
+    }
 }
 
 /// /// An anonymous identifier, optionally with a prefix.
@@ -26,26 +47,20 @@ pub struct AnonName {
     pub(crate) index: usize,
 }
 
+/// Only given identifiers can be equal; anonymous ones have no identifying information, so are assumed non-equal.
+impl PartialEq for AnonName {
+    fn eq(&self, other: &Self) -> bool {
+        return false
+    }
+}
+
 #[derive(Debug, Clone)]
 pub(crate) enum InputName {
     Given(GivenName),
     Anonymous(AnonName),
 }
 
-/// Only given identifiers can be equal; anonymous ones have no identifying information, so are assumed non-equal.
-impl PartialEq for GivenName {
-    fn eq(&self, other: &Self) -> bool {
-        self.index == other.index
-    }
-}
 
-impl Eq for GivenName {}
-
-impl hash::Hash for GivenName {
-    fn hash<H: hash::Hasher>(&self, state: &mut H) {
-        self.index.hash(state)
-    }
-}
 
 
 #[cfg(test)]
@@ -83,25 +98,10 @@ mod given {
     use super::*;
 
     #[test]
-    fn different_variant() {
-        unimplemented!();
-        // let mut scope = RootScope::new_root();
-        // let name1 = scope.add_named("hello").unwrap();
-        // let name2 = scope.add_named("world").unwrap();
-        // assert_eq!(name1, name1);
-        // assert_eq!(name2, name2.clone());
-        // assert_ne!(name1, name2);
-        // let mut set = HashSet::new();
-        // assert!(set.insert(name1.clone()));
-        // assert!(set.insert(name2));
-        // assert!(!set.insert(name1));
-    }
-
-    #[test]
     fn different_text() {
         let mut scope = RootScope::new_root();
-        let name1 = scope.add_named("hello").unwrap();
-        let name2 = scope.add_named("world").unwrap();
+        let name1 = scope.add_named("hello").unwrap().unwrap_given();
+        let name2 = scope.add_named("world").unwrap().unwrap_given();
         assert_eq!(name1, name1);
         assert_eq!(name2, name2.clone());
         assert_ne!(name1, name2);
@@ -115,9 +115,9 @@ mod given {
     fn different_scope() {
         let mut scope = RootScope::new_root();
         let child1 = scope.add_child();
-        let name1 = child1.add_named("hello").unwrap();
+        let name1 = child1.add_named("hello").unwrap().unwrap_given();
         let child2 = scope.add_child();
-        let name2 = child2.add_named("hello").unwrap();
+        let name2 = child2.add_named("hello").unwrap().unwrap_given();
         assert_eq!(name1, name1);
         assert_eq!(name2, name2.clone());
         assert_ne!(name1, name2);
@@ -131,8 +131,8 @@ mod given {
     fn different_root() {
         let mut scope1 = RootScope::new_root();
         let mut scope2 = RootScope::new_root();
-        let name1 = scope1.add_named("hello").unwrap();
-        let name2 = scope2.add_named("hello").unwrap();
+        let name1 = scope1.add_named("hello").unwrap().unwrap_given();
+        let name2 = scope2.add_named("hello").unwrap().unwrap_given();
         assert_eq!(name1, name1);
         assert_eq!(name2, name2.clone());
         assert_ne!(name1, name2);
